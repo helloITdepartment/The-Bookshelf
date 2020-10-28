7b439a6db5386b1537b5fec77c0c113801e27723
Jacques Benzakein <Jackbenzakein@gmail.com>
2020-10-28 19:09:58 +0200

    Added a collectionView into the lowerView that's meant to go sideways and display a list of saved locations for the user to chose from

---
+++ The Bookshelf/Models/Location.swift
@@ -0,0 +1,27 @@
+//
+//  Location.swift
+//  The Bookshelf
+//
+//  Created by Jacques Benzakein on 10/26/20.
+//  Copyright © 2020 Q Technologies. All rights reserved.
+//
+
+import Foundation
+
+struct Location {
+    
+    typealias Shelf = (Int, Int)
+    
+    var name: String
+    var shelf: Shelf?
+    
+    
+}
+
+extension Location: Equatable {
+    
+    static func == (lhs: Location, rhs: Location) -> Bool {
+        lhs.name == rhs.name
+    }
+    
+}
--- The Bookshelf/Views/Manual Entry Cell subclasses/TBOptionEntryCVCell.swift
+++ The Bookshelf/Views/Manual Entry Cell subclasses/TBOptionEntryCVCell.swift
@@ -14,7 +14,48 @@ class TBOptionEntryCVCell: TBManualEntryCollectionViewCell {
 
     var helperVCPresenterDelegate: HelperVCPresenterDelegate!
 
+    var collectionView: UICollectionView!
+    
+    let locations = [nil, "Main room", "Guest bedroom", "Downstairs"]
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+
+    override func configureLowerView() {
+        super.configureLowerView()
+        
+        DispatchQueue.main.async {
+            self.configureCollectionView()
+        }
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
     func set(labelText: String) {
         titleLabel.text = labelText
     }
+    
+    private func configureCollectionView() {
+    
+        collectionView = UICollectionView(frame: lowerView.bounds, collectionViewLayout: UICollectionView.createHorizontalFlowLayout(for: lowerView.frame.height))
+        collectionView.backgroundColor = .tertiarySystemBackground
+        collectionView.layer.cornerRadius = Constants.smallItemCornerRadius
+        lowerView.addSubview(collectionView)
+    }
+}
+
+extension TBOptionEntryCVCell: UICollectionViewDataSource {
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        locations.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        <#code#>
+    }
+    
+    
 }
